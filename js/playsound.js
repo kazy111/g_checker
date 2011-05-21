411987a8a1d5f8baa9ffc37cbef9562118815643
Kazy <kazy@kazy111.info>
2011-05-21 10:52:02 +0900

    add 2 welcome song and file selector (temporary)

--- js/playsound.js
+++ js/playsound.js
@@ -1,10 +1,11 @@
+var arr = new Array('welcome', 'theme', 'theme2');
 
 if (document.createElement) window.onload = function() {
   var ele = document.createElement('div');
   var id = '_playsound';
   ele.id = id;
   document.body.appendChild(ele);
-  swfobject.embedSWF('js/playmp3.swf?url=media/welcome.mp3', id, 1, 1, '8.0.0');
+  swfobject.embedSWF('js/playmp3.swf?url=media/'+arr[Math.floor(Math.random()*arr.length)]+'.mp3', id, 1, 1, '8.0.0');
 
   //var play = document.getElementById('_playsound');
   //play.playFile('media/welcome.mp3')
