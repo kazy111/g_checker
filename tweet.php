0c0a7888acaad9e64f64e46dcc5aee8628b1de66
kazy111 <kazy@kazy111.info>
2012-10-13 01:45:14 +0900

    change Twitter API to 1.1

--- tweet.php
+++ tweet.php
@@ -10,7 +10,7 @@ function tweet($str)
   /* Run request on twitter API as user. */
   //$content = $to->OAuthRequest('https://twitter.com/account/verify_credentials.xml', array(), 'GET');
   print("\nTweet: ".$str."\n");
-  $content = $to->OAuthRequest('https://twitter.com/statuses/update.xml',
+  $content = $to->OAuthRequest('https://api.twitter.com/1.1/statuses/update.json',
                                'POST', array('status' => $str));
   //$content = $to->OAuthRequest('https://twitter.com/statuses/replies.xml', array(), 'POST');
   print($content);
